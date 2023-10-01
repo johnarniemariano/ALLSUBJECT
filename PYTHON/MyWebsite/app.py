c5cb8ed798d29622a08dcb4eca5fd302140570cb
John Arnie A. Mariano <138758068+johnarniemariano@users.noreply.github.com>
2023-10-01 12:59:08 +0800

    Update app.py

--- PYTHON/MyWebsite/app.py
+++ PYTHON/MyWebsite/app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/favorites")
+def favorites():
+    return render_template("favorites.html")
+
+@app.route("/friends")
+def friends():
+    return render_template("friends.html")
+
+@app.route("/family")
+def family():
+    return render_template("family.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
