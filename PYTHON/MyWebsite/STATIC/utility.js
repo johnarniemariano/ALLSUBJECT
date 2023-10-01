9317e9517e9232cd499ffc522ebb578e42f2eced
John Arnie A. Mariano <138758068+johnarniemariano@users.noreply.github.com>
2023-10-01 13:00:30 +0800

    Add files via upload

---
+++ PYTHON/MyWebsite/STATIC/utility.js
@@ -0,0 +1,5 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code goes here
+    // For example, you can add event listeners or perform actions on page load.
+});
